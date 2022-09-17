2a7fccff016032ad393caa368dacfaba95d49a72
Mustafa Köroğlu <m.mustafakoroglu@gmail.com>
2022-09-17 09:19:40 +0300

    Add Spring Boot app

---
+++ src/main/java/com/mustafakoroglu/springwithdesignpatterns/SpringWithDesignPatternsApplication.java
@@ -0,0 +1,12 @@
+package com.mustafakoroglu.springwithdesignpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithDesignPatternsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringWithDesignPatternsApplication.class, args);
+  }
+}
